fd0390ba089c6df22e98e931d8066eedbe6e727b
jenidub <stanfordjeni@gmail.com>
2022-10-19 21:51:53 -0700

    bugfix placeholder attempt 3

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -51,7 +51,7 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image !== "" ? this.props.item.image : placeholder}
+                src={this.props.item.image ? this.props.item.image : placeholder}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image !== "" ? item.image : placeholder}
+        src={item.image ? item.image : placeholder}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
